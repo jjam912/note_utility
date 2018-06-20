341590c05403e3a9ed5f5e3cf36c22261a217148
James S. Wang <new.jjam912@gmail.com>
2018-06-20 17:34:35 -0400

    Added notations module

---
+++ .idea/notations.py
@@ -0,0 +1,11 @@
+
+class Term:
+
+    def __init__(self, categories, ):
+        self.categories = categories
+
+
+class Definition:
+
+    def __init__(self, categories, ):
+        self.categories = categories
