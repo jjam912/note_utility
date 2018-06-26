e327e7dcfb19908a34855f1fcc24368c70ea5b2b
James S. Wang <new.jjam912@gmail.com>
2018-06-25 20:49:42 -0400

    Created Term and Definition class.

--- notations.py
+++ notations.py
@@ -1,11 +1,27 @@
 
 class Term:
 
-    def __init__(self, categories, ):
-        self.categories = categories
+    def __init__(self, text: str, index: int,):
+        self.text = text
+        self.index = index
+
+    def __str__(self):
+        message = "Term: " + self.text
+        return message
 
 
 class Definition:
 
-    def __init__(self, categories, ):
-        self.categories = categories
+    def __init__(self, text: str, index: int,):
+        self.text = text
+        self.index = index
+
+    def __str__(self):
+        message = "Definition: " + self.text
+        return message
+
+
+class Category:
+    pass
+
+
