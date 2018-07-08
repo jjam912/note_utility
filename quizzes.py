94cb33cab2606361ba409b74fc13a9c2d736d0d0
James S. Wang <new.jjam912@gmail.com>
2018-07-08 16:23:52 -0400

    This module will replace index pointers in NoteUtil as those are mainly used for quizzing.

---
+++ quizzes.py
@@ -0,0 +1,33 @@
+class Quiz:
+    def __init__(self, noteutil):
+        self.noteutil = noteutil
+        self.correct = []
+        self.incorrect = []
+        self.important = []
+
+    def question(self):
+        pass
+
+    def add_correct(self):
+        pass
+
+    def add_incorrect(self):
+        pass
+
+    def add_important(self):
+        pass
+
+    def remove_correct(self):
+        pass
+
+    def remove_incorrect(self):
+        pass
+
+    def remove_important(self):
+        pass
+
+    def define(self, *, index, term):
+        pass
+
+    def lookup(self, *, index, term, definition):
+        pass
