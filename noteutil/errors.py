3a1e68283a03c3c80c7eeaa62c11f856543d38ce
James S. Wang <new.jjam912@gmail.com>
2018-09-27 15:49:31 -0400

    Added NoArgsPassed and removed previous errors

--- noteutil/errors.py
+++ noteutil/errors.py
@@ -6,51 +6,11 @@ class NotesNotFound(NotesError):
     pass
 
 
-class NotesIndexError(NotesNotFound):
+class NotesIndexError(NotesError):
     pass
 
 
-class ForbiddenEdit(NotesError):
-    pass
-
-
-class DuplicateError(NotesError):
-    pass
-
-
-class MultipleFoundError(NotesError):
-    pass
-
-
-class DelimeterError(NotesError):
-    pass
-
-
-class MissingDelimeterError(DelimeterError):
-    pass
-
-
-class ExtraDelimeterError(DelimeterError):
-    pass
-
-
-class NoDefinitionError(NotesNotFound):
-    pass
-
-
-class DuplicateTermError(DuplicateError):
-    pass
-
-
-class MissingBoundError(NotesError):
-    pass
-
-
-class NoCategoryError(NotesNotFound):
-    pass
-
-
-class NoExtensionError(NotesNotFound):
+class NoArgsPassed(NotesError):
     pass
 
 
