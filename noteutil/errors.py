929180662469536433b0f13270663ab2a73069e7
James S. Wang <new.jjam912@gmail.com>
2018-09-28 23:05:10 -0400

    Added LineNotFound and LineIndexError to errors

--- noteutil/errors.py
+++ noteutil/errors.py
@@ -1,22 +1,22 @@
-class NotesError(Exception):
+class NoteUtilError(Exception):
     """Base exception for all ``noteutil`` modules."""
     
     pass
 
 
-class NotesNotFound(NotesError):
+class NoteNotFound(NoteUtilError):
     """This exception is thrown when no `Note`_ is found in the ``notes_list`` with the given arguments."""
     
     pass
 
 
-class NotesIndexError(NotesError):
+class NoteIndexError(NoteUtilError):
     """This exception is thrown when a ``nindex`` is passed that is out of range of the ``notes_list``."""
     
     pass
 
 
-class NoArgsPassed(NotesError):
+class NoArgsPassed(NoteUtilError):
     """This exception is thrown when no arguments are passed into a function of any `NoteUtil`_ that needs at least one.
     
     If all of the arguments of a function are ``None``, this exception will be thrown.
@@ -25,7 +25,7 @@ class NoArgsPassed(NotesError):
     pass
 
 
-class LineExpected(NotesError):
+class LineExpected(NoteUtilError):
     """This exception is thrown when a `Line`_ or subclass of `Line`_ was expected from the ``notes_list``.
     
     This should theoretically never happen because all `Note`_\ s automatically convert to `Line`_\ s.
@@ -33,3 +33,10 @@ class LineExpected(NotesError):
     
     pass
 
+
+class LineNotFound(NoteNotFound):
+    pass
+
+
+class LineIndexError(NoteIndexError):
+    pass
