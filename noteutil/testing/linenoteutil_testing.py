da90d45df419299ac199d54a3e8026f616b6c402
James S. Wang <new.jjam912@gmail.com>
2018-10-01 20:16:21 -0400

    Added more test cases

--- noteutil/testing/linenoteutil_testing.py
+++ noteutil/testing/linenoteutil_testing.py
@@ -1,20 +1,29 @@
 from noteutil.noteutil.noteutil import LineNoteUtil
 
 nu = LineNoteUtil("test1.txt", comment="#")
-print(nu)
+# print(nu)
 
 print(nu.nindex(lindex=0))
 print(nu.nindex(content="TEST1: TEST2"))
+print(nu.nindex(lindex=0, content="TEST1: TEST2"))
+
 print(nu.nindexes(content="Data"))
-print(nu.nindexes(lindexes=[2, 3, 4, 5]))
+print(nu.nindexes(lindexes=[6, 7, 8, 9]))
+print(nu.nindexes(content="Data", lindexes=[6, 7, 8, 9]))
+
 print(nu.lindex(content="Data2: 5%93%"))
 print(nu.lindex(nindex=3))
+print(nu.lindex(content="Data2: 5%93%", nindex=3))
+
 print(nu.line(content="~~ Category 2 In category 1"))
 print(nu.line(nindex=6))
 print(nu.line(lindex=6))
+print(nu.line(content="~~ Category 2 In category 1", nindex=6, lindex=6))
+
 print(nu.lines(content="9"))
-print(nu.lines(nindexes=[3, 4, 8]))
-print(nu.lines(lindexes=[3, 4, 8]))
+print(nu.lines(nindexes=[6, 7, 9]))
+print(nu.lines(lindexes=[1, 2, 5]))
+print(nu.lines(content="9", nindexes=[1, 2, 5], lindexes=[6, 7, 9]))
 
 # print(nu.nindex())
 # print(nu.nindex())
