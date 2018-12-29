fdf2d6428c9e651c0f2213b2be31621ef87c9961
jjam912 <new.jjam912@gmail.com>
2018-12-29 15:20:12 -0500

    Removed numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,6 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
