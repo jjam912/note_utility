c2dd3787339c21290f71060ca8b160ab26a0dd94
jjam912 <new.jjam912@gmail.com>
2018-12-29 16:08:40 -0500

    Changed documentation to fit numpy's more accurately

--- noteutil/notes.py
+++ noteutil/notes.py
@@ -8,29 +8,29 @@ class Note(abc.ABC):
     """Base class for all notes, which are created when parsing the note file."""
 
     # Order of constructor args: content, hidden content, outside content, outside hidden content.
-    def __init__(self, content: str, nindex: int, rcontent: str, prefix: str,
-                 extensions: dict, categories: list, tabs: int, fmt):
+    def __init__(self, content, nindex, rcontent, prefix, extensions, categories, tabs, fmt):
         """
         Parameters
         ----------
-        content : :class:`str`
-            The content of the note excluding prefixes and extensions.
-        nindex : :class:`int`
-            The `Note` index that corresponds to the position in the :attr:`NoteUtil.notes_list`.
-        rcontent : :class:`str`
-            Raw content of the `Note`.
-        prefix : :class:`str`
-            Any prefixes this `Note` had to determine type of `Note` or `Category`.
-        extensions: Dict[:class:`str` : :class:`Extension`]
-            A dict of the extensions that this `Note` has with key: :attr:`Extension.name` and value :class:`Extension`.
-        categories: List[:class:`Category`]
-            An unsorted list of all of the categories this `Note` belongs to.
-        tabs: :class:`int`
-            The number of tabs that indicates how nested this `Note` is (one per category).
+        content : str
+            The content of the Note excluding prefixes and extensions.
+        nindex : int
+            The Note index that corresponds to the position in the :attr:`NoteUtil.notes_list`.
+        rcontent : str
+            Raw content of the Note.
+        prefix : str
+            Any prefixes this Note had that were used to determine type of Note or Category.
+        extensions: dict of {str : :class:`Extension`}
+            A dict of the Extensions that this Note has with key: :attr:`Extension.name` and value: :class:`Extension`.
+        categories: list of :class:`Category`
+            An unsorted list of all of the Categories this Note belongs to.
+        tabs: int
+            The number of tabs that indicates how nested this Note is (one per Category).
         fmt: function
-            The function applied to a `Note` to give it its `str` value.
+            The function applied to a Note to give it its `str` value.
             Must take a `Note` as a parameter.
         """
+
         super().__init__()
         self.content = content
         self.nindex = nindex
@@ -64,26 +64,26 @@ class Note(abc.ABC):
         pass
 
 
-# A single token of notes refers to the string that is in between the NoteUtil's separators in the .nu file.
 class Line(Note):
-    """A single token of notes.
+    """A single token of text.
+
+    This is all of the text that is found in between two of a NoteUtil's separators in the .nu file.
 
     Attributes
     ----------
-    content: :class:`str`
-        The actual content of the Line; all of the text that was found in between separators.
-    nindex: :class:`int`
-        The `Note` index that corresponds to the position in the :attr:`NoteUtil.notes_list`.
-    lindex: :class:`int`
-        The `Line` index that corresponds to the position in the :attr:`NoteUtil.lines_list`.
-    extensions: Dict[:class:`str` : :class:`Extension`]
-        A dict of the extensions that this `Line` has with key: :attr:`Extension.name` and value :class:`Extension`.
-    categories: List[:class:`Category`]
-        An unsorted list of all of the categories this `Line` belongs to.
+    content: str
+        The actual content of the Line; all of the text that was found in between the separators.
+    nindex: int
+        The Note index that corresponds to the position in the :attr:`NoteUtil.notes_list`.
+    lindex: int
+        The Line index that corresponds to the position in the :attr:`NoteUtil.lines_list`.
+    extensions: dict of {str :class:`Extension`}
+        A dict of the Extensions that this Line has with key: :attr:`Extension.name` and value :class:`Extension`.
+    categories: list of :class:`Category`
+        An unsorted list of all of the Categories this Line belongs to.
     """
 
-    def __init__(self, content: str, nindex: int, lindex: int, rcontent: str, prefix: str,
-                 extensions: dict, categories: list, tabs, fmt):
+    def __init__(self, content, nindex, lindex, rcontent, prefix, extensions, categories, tabs, fmt):
         super().__init__(content, nindex, rcontent, prefix, extensions, categories, tabs, fmt)
         self.lindex = lindex
 
@@ -98,33 +98,33 @@ class Line(Note):
         return rstring
 
 
-# A Note token will automatically become a Pair if its separator is in the rcontent
-# unless it has the prefix of a LineGroup.
 class Pair(Note):
     """A single token of notes with a separator that splits a term and a definition.
 
+    A `Note` will automatically become a `Pair` if its `separator` is in the `rcontent` of the `Note`.
+    This holds true unless the `rcontent` contains the prefix of a `LineGroup`.
+
     Attributes
     ----------
-    content: :class:`str`
-        The actual content of the notes; all of the text that was found in between separators.
-    nindex: :class:`int`
-        The note index that corresponds to the position in the :attr:`NoteUtil.notes_list`.
-    pindex: :class:`int`
-        The pair index that corresponds to the position in the :attr:`NoteUtil.pairs_list`.
-    separator: :class:`str`
-        The string that separates the key and value (term and definition) of the notes token.
-    term: :class:`str`
-        The first part that came before the separator.
+    content: str
+        The actual content of the Notes; all of the text that was found in between separators.
+    nindex: int
+        The Note index that corresponds to the position in the :attr:`NoteUtil.notes_list`.
+    pindex: int
+        The Pair index that corresponds to the position in the :attr:`NoteUtil.pairs_list`.
+    separator: str
+        The string that separates the key and value (term and definition) of the Note token.
+    term: str
+        The first part of text that came before the `separator`.
     definition: :class:`str`
-        The second part that came after the separator.
-    extensions: Dict[:class:`str` : :class:`Extension`]
-        A dict of the extensions that this `Pair` has with key: :attr:`Extension.name` and value :class:`Extension`.
-    categories: List[:class:`Category`]
-        An unsorted list of all of the categories this `Note` belongs to.
+        The second part of text that came after the `separator`.
+    extensions: dict of {str :class:`Extension`}
+        A dict of the Extensions that this Pair has with key: :attr:`Extension.name` and value :class:`Extension`.
+    categories: list of :class:`Category`
+        An unsorted list of all of the categories this Pair belongs to.
     """
 
-    def __init__(self, content: str, nindex: int, pindex: int, separator: str, rcontent: str, prefix: str,
-                 extensions: dict, categories: list, tabs, fmt):
+    def __init__(self, content, nindex, pindex, separator, rcontent, prefix, extensions, categories, tabs, fmt):
         super().__init__(content, nindex, rcontent, prefix, extensions, categories, tabs, fmt)
         self.pindex = pindex
         self.separator = separator
