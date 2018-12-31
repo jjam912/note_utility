6454407e1857dd4b33441b68ddc57b55634310ae
jjam912 <new.jjam912@gmail.com>
2018-12-31 14:19:47 -0500

    Moved around classes, changed some docs

--- noteutil/categories.py
+++ noteutil/categories.py
@@ -32,11 +32,53 @@ class Category(abc.ABC):
         return note in self.notes
 
 
-class GlobalCategory(Category):
-    """A `GlobalCategory` is a `Category` that is not affected by the position of a `Note`.
+class PositionalCategory(Note, Category):
+    """A `PositionalCategory` is a `Category` whose position is important to the classification of its `Notes`.
 
-    An example of a `GlobalCategory` would be People, such as different important people throughout history.
-    There could be important people several time periods, so it's illogical to separate them and group them together.
+    This class acts similar to a `Line`.
+
+    Attributes
+    ----------
+    content : str
+        The content of the `Category` excluding `prefixes` and `Extensions`.
+    nindex : int
+        The `Note` index that corresponds to the position in the `NoteUtil.notes`.
+    pindex : int
+        The `Positional` index that corresponds to the position in the `NoteUtil.positionals`.
+    name : str
+        The name assigned to this `Category`, used as a key in `NoteUtil.categories`.
+    removed : bool
+        Whether `Notes` of this `Category` were removed from the `NoteUtil.notes`.
+    notes : list of `Note`
+        A sorted list of all of the `Notes` that belong to this `Category`.
+    lines : list of `Line`
+        A sorted list of all of the `Lines` that belong to this `Category`.
+    pairs : list of `Pair`
+        A sorted list of all of the `Pairs` that belong to this `Category`.
+    extensions : dict of {str : `Extension`}
+        A dict of the `Extensions` that this `Line` has with key: `Extension.name` and value `Extension`.
+    categories : list of `Category`
+        A sorted list of all of the `Categories` this `Line` belongs to.
+    """
+
+    def __init__(self, content, nindex, pindex, name, rcontent, prefix, extensions, categories, tabs, fmt):
+        super().__init__(content, nindex, rcontent, prefix, extensions, categories, tabs, fmt)
+        super().__init__(name, prefix, fmt)     # The format of the `Note` will be overridden by the `Category` one.
+        self.pindex = pindex
+
+    def __repr__(self):
+        rstring = ""
+        rstring += self._prefix
+        rstring += self.content
+        for extension in self.extensions:
+            rstring += repr(extension)
+        for category in self.categories[::-1]:
+            rstring = category._prefix + rstring
+        return rstring
+
+
+class GlobalCategory(Category):
+    """A `GlobalCategory` is a `Category` whose `Note's` position is irrelevant.
 
     Attributes
     ----------
@@ -55,50 +97,3 @@ class GlobalCategory(Category):
     def __init__(self, name, removed, prefix, fmt):
         super().__init__(name, prefix, fmt)
         self.removed = removed
-
-
-class PositionalCategory(Note, Category):
-    def __init__(self, content, nindex, pindex, name, rcontent, prefix, extensions, categories, tabs, fmt):
-        """A `PositionalCategory` is a `Category` whose position is important to the classification of its `Notes`.
-
-        An example would be if your notes are in chronological order and
-            all notes that follow a `Category` belong to that `Category`.
-        This class acts similar to a `Line`.
-
-        Attributes
-        ----------
-        content : str
-            The content of the `Category` excluding `prefixes` and `Extensions`.
-        nindex : int
-            The `Note` index that corresponds to the position in the `NoteUtil.notes`.
-        pindex : int
-            The `Positional` index that corresponds to the position in the `NoteUtil.positionals`.
-        name : str
-            The name assigned to this `Category`, used as a key in `NoteUtil.categories`.
-        removed : bool
-            Whether `Notes` of this `Category` were removed from the `NoteUtil.notes`.
-        notes : list of `Note`
-            A sorted list of all of the `Notes` that belong to this `Category`.
-        lines : list of `Line`
-            A sorted list of all of the `Lines` that belong to this `Category`.
-        pairs : list of `Pair`
-            A sorted list of all of the `Pairs` that belong to this `Category`.
-        extensions : dict of {str : `Extension`}
-            A dict of the `Extensions` that this `Line` has with key: `Extension.name` and value `Extension`.
-        categories : list of `Category`
-            A sorted list of all of the `Categories` this `Line` belongs to.
-        """
-
-        super().__init__(content, nindex, rcontent, prefix, extensions, categories, tabs, fmt)
-        super().__init__(name, prefix, fmt)     # The format of the `Note` will be overridden by the `Category` one.
-        self.pindex = pindex
-
-    def __repr__(self):
-        rstring = ""
-        rstring += self._prefix
-        rstring += self.content
-        for extension in self.extensions:
-            rstring += repr(extension)
-        for category in self.categories[::-1]:
-            rstring = category._prefix + rstring
-        return rstring
