7b355218360370c3ffc052e1e7e4536f23ff9566
jjam912 <new.jjam912@gmail.com>
2018-12-31 12:38:10 -0500

    Added more documentation and moved it to  docstring.

--- noteutil/notes.py
+++ noteutil/notes.py
@@ -5,33 +5,30 @@ from .errors import *
 # The plan is that we will create everything needed to pass into the Note's constructor
 # in the NoteUtil method and then just make the Note.
 class Note(abc.ABC):
+    """A `Note` is the text found in the notes file. It contains the actual notes.
+
+    Parameters
+    ----------
+    content : str
+        The content of the `Note` excluding `prefixes` and `Extensions`.
+    nindex : int
+        The `Note` index that corresponds to the position in the `NoteUtil.notes`.
+    rcontent : str
+        Raw content of the `Note`.
+    prefix : str
+        Any prefixes this `Note` had that were used to determine the type of `Category`.
+    extensions: dict of {str : `Extension`}
+        A dict of the `Extensions` this `Note` has with key: `Extension.name` and value: `Extension`.
+    categories: list of `Category`
+        A sorted list of all of the `Categories` this `Note` belongs to.
+    tabs: int
+        The number of tabs that indicates how nested this `Note` is (one per `Category`).
+    fmt: function
+        The function applied to a `Note` to give it its `str` value.
+    """
 
     # Order of constructor args: content, hidden content, outside content, outside hidden content.
     def __init__(self, content, nindex, rcontent, prefix, extensions, categories, tabs, fmt):
-        """
-        Base class for all notes, which are created when parsing the note file.
-
-        Parameters
-        ----------
-        content : str
-            The content of the `Note` excluding `prefixes` and `Extensions`.
-        nindex : int
-            The `Note` index that corresponds to the position in the `NoteUtil.notes_list`.
-        rcontent : str
-            Raw content of the `Note`.
-        prefix : str
-            Any prefixes this `Note` had that were used to determine the type of `Note` or `Category`.
-        extensions: dict of {str : `Extension`}
-            A dict of the `Extensions` this `Note` has with key: `Extension.name` and value: `Extension`.
-        categories: list of `Category`
-            An unsorted list of all of the `Categories` this `Note` belongs to.
-        tabs: int
-            The number of tabs that indicates how nested this `Note` is (one per `Category`).
-        fmt: function
-            The function applied to a `Note` to give it its `str` value.
-            Must take a `Note` as a parameter.
-        """
-
         super().__init__()
         self.content = content
         self.nindex = nindex
@@ -72,16 +69,16 @@ class Line(Note):
 
     Attributes
     ----------
-    content: str
-        The actual content of the `Line`; all of the text that was found in between the `separators`.
-    nindex: int
-        The `Note` index that corresponds to the position in the `NoteUtil.notes_list`.
-    lindex: int
-        The `Line` index that corresponds to the position in the `NoteUtil.lines_list`.
-    extensions: dict of {str : `Extension`}
+    content : str
+        The content of the `Line` excluding `prefixes` and `Extensions`.
+    nindex : int
+        The `Note` index that corresponds to the position in the `NoteUtil.notes`.
+    lindex : int
+        The `Line` index that corresponds to the position in the `NoteUtil.lines`.
+    extensions : dict of {str : `Extension`}
         A dict of the `Extensions` that this `Line` has with key: `Extension.name` and value `Extension`.
-    categories: list of `Category`
-        An unsorted list of all of the `Categories` this `Line` belongs to.
+    categories : list of `Category`
+        A sorted list of all of the `Categories` this `Line` belongs to.
     """
 
     def __init__(self, content, nindex, lindex, rcontent, prefix, extensions, categories, tabs, fmt):
@@ -90,39 +87,36 @@ class Line(Note):
 
     def __repr__(self):
         rstring = ""
+        rstring += self._prefix
         rstring += self.content
-        for ext in self.extensions:
-            rstring += repr(ext)
-        for cat in reversed(self.categories):
-            if isinstance(cat, GlobalCategory):
-                rstring = cat.prefix + rstring
+        for extension in self.extensions:
+            rstring += repr(extension)
+        for category in self.categories[::-1]:
+            rstring = category._prefix + rstring
         return rstring
 
 
 class Pair(Note):
     """A single token of notes with a separator that splits a term and a definition.
 
-    A `Note` will automatically become a `Pair` if its `separator` is in the `rcontent` of the `Note`.
-    This holds true unless the `rcontent` contains the `prefix` of a `LineGroup`.
-
     Attributes
     ----------
-    content: str
-        The actual content of the `Notes`; all of the text that was found in between `separators`.
-    nindex: int
-        The `Note` index that corresponds to the position in the `NoteUtil.notes_list`.
-    pindex: int
-        The `Pair` index that corresponds to the position in the `NoteUtil.pairs_list`.
-    separator: str
+    content : str
+        The content of the `Pair` excluding `prefixes` and `Extensions`.
+    nindex : int
+        The `Note` index that corresponds to the position in the `NoteUtil.notes`.
+    pindex : int
+        The `Pair` index that corresponds to the position in the `NoteUtil.pairs`.
+    separator : str
         The string that separates the key and value (term and definition) of the `Note` token.
-    term: str
+    term : str
         The first part of text that came before the `separator`.
-    definition: str
+    definition : str
         The second part of text that came after the `separator`.
-    extensions: dict of {str : `Extension`}
+    extensions : dict of {str : `Extension`}
         A dict of the `Extensions` that this `Pair` has with key: `Extension.name` and value `Extension`.
-    categories: list of `Category`
-        An unsorted list of all of the `Categories` this `Pair` belongs to.
+    categories : list of `Category`
+        A sorted list of all of the `Categories` this `Pair` belongs to.
     """
 
     def __init__(self, content, nindex, pindex, separator, rcontent, prefix, extensions, categories, tabs, fmt):
