44c03c388c73c3b7c314665906ae411a8d51ab70
jjam912 <new.jjam912@gmail.com>
2019-03-30 09:15:53 -0400

    Added docs to the functions

--- noteutil2/comparisons.py
+++ noteutil2/comparisons.py
@@ -2,16 +2,44 @@
 
 
 def is_equal(note, **kwargs):
+    """Is True when all of the `kwargs` values are exactly equal to the `Note`'s attributes.
+
+    Returns
+    -------
+    bool
+    """
+
     return all(getattr(note, attr) is not None and val == getattr(note, attr) for attr, val in kwargs.items())
 
 
 def is_in(note, **kwargs):
+    """Is True when all of the `kwargs` values are "in" the `Note`'s attributes.
+
+    Returns
+    -------
+    bool
+    """
+
     return all(getattr(note, attr) is not None and val in getattr(note, attr) for attr, val in kwargs.items())
 
 
 def is_similar(note, **kwargs):
+    """Is True when all of the `kwargs` values are equal ignoring case to the `Note`'s attributes.
+
+    Returns
+    -------
+    bool
+    """
+
     return all(getattr(note, attr) is not None and val.lower() == getattr(note, attr).lower() for attr, val in kwargs.items())
 
 
 def is_in_similar(note, **kwargs):
+    """Is True when all of the `kwargs` values are "in" ignoring case to the `Note`'s attributes.
+
+    Returns
+    -------
+    bool
+    """
+
     return all(getattr(note, attr) is not None and val.lower() in getattr(note, attr).lower() for attr, val in kwargs.items())
