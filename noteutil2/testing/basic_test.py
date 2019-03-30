9cd8d50cc98f7f977e4889221c6e245b1e106755
jjam912 <new.jjam912@gmail.com>
2019-03-30 08:24:25 -0400

    Updated testing for get methods

--- noteutil2/testing/basic_test.py
+++ noteutil2/testing/basic_test.py
@@ -1,5 +1,11 @@
 from noteutil.noteutil2.noteutil import NoteUtil
+from noteutil.noteutil2.comparisons import is_equal, is_in, is_similar, is_in_similar
 
 
 noteutil = NoteUtil("basic_config.txt")
 print(noteutil)
+print(noteutil.get(content="Line 1", nindex=0))
+print(list(map(str, noteutil.get_list(content="Line 5", compare=is_equal))))
+print(list(map(str, noteutil.get_list(content="Pair", compare=is_in))))
+print(list(map(str, noteutil.get_list(definition="pair", compare=is_similar))))
+print(list(map(str, noteutil.get_list(content="line", compare=is_in_similar))))
