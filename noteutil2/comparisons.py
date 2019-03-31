fe932fa981f4541adcd9c8cd0600c1562833b934
jjam912 <new.jjam912@gmail.com>
2019-03-31 13:09:05 -0400

    Added Enum for CompareOptions so ugly functions can be avoided.

--- noteutil2/comparisons.py
+++ noteutil2/comparisons.py
@@ -1,4 +1,5 @@
-"""This module is **Temporarily** for custom comparisons used when comparing `Note`s in `NoteUtil`."""
+"""This module is for comparisons used when comparing `Note`s in `NoteUtil`."""
+import enum
 
 
 def is_equal(note, **kwargs):
@@ -12,17 +13,6 @@ def is_equal(note, **kwargs):
     return all(getattr(note, attr) is not None and val == getattr(note, attr) for attr, val in kwargs.items())
 
 
-def is_in(note, **kwargs):
-    """Is True when all of the `kwargs` values are "in" the `Note`'s attributes.
-
-    Returns
-    -------
-    bool
-    """
-
-    return all(getattr(note, attr) is not None and val in getattr(note, attr) for attr, val in kwargs.items())
-
-
 def is_similar(note, **kwargs):
     """Is True when all of the `kwargs` values are equal ignoring case to the `Note`'s attributes.
 
@@ -34,7 +24,18 @@ def is_similar(note, **kwargs):
     return all(getattr(note, attr) is not None and val.lower() == getattr(note, attr).lower() for attr, val in kwargs.items())
 
 
-def is_in_similar(note, **kwargs):
+def is_in(note, **kwargs):
+    """Is True when all of the `kwargs` values are "in" the `Note`'s attributes.
+
+    Returns
+    -------
+    bool
+    """
+
+    return all(getattr(note, attr) is not None and val in getattr(note, attr) for attr, val in kwargs.items())
+
+
+def is_similar_in(note, **kwargs):
     """Is True when all of the `kwargs` values are "in" ignoring case to the `Note`'s attributes.
 
     Returns
@@ -43,3 +44,28 @@ def is_in_similar(note, **kwargs):
     """
 
     return all(getattr(note, attr) is not None and val.lower() in getattr(note, attr).lower() for attr, val in kwargs.items())
+
+
+class CompareOptions(enum.Enum):
+    """Enum for selecting compare options for NoteUtil.get methods.
+
+    Constants
+    ---------
+    EQUALS : function
+        Tests for equality. Use with ints, or when you want exact matching.
+    SIMILAR : function
+        Tests for equals ignore case. Use with strings when you want to match, but ignore case.
+    IN : function
+        Tests for "in". Use with strings when you want to know if the exact value is in the attribute.
+    SIN : function
+        Tests for "in" and also ignores case. Use with strings when you want to know if the value is in the attribute.
+        Stands for "Similar In".
+
+    """
+
+    EQUALS = is_equal
+    SIMILAR = is_similar
+    IN = is_in
+    SIN = is_similar_in
+
+
