4855983bed1396ca4d63ea59864bc3b1b4f18157
jjam912 <new.jjam912@gmail.com>
2019-03-31 10:33:12 -0400

    Added tests to headings

--- noteutil2/testing/heading_test.py
+++ noteutil2/testing/heading_test.py
@@ -1,5 +1,5 @@
-from noteutil.noteutil2.noteutil import NoteUtil
-from noteutil.noteutil2.comparisons import is_equal, is_in, is_similar, is_in_similar
+from noteutil2.noteutil import NoteUtil
+from noteutil2.comparisons import is_equal, is_in, is_similar, is_in_similar
 
 
 def test_noteutil(noteutil):
@@ -11,10 +11,28 @@ def test_noteutil(noteutil):
     string += "Separator: " + str(noteutil.separator) + "\n"
     string += "Notes List" + "\n"
     string += "----------" + "\n"
-    for note in noteutil.notes_list:
+    for note in noteutil.notes:
         string += "\t" + test_note(note) + "\n"
     string += "----------" + "\n"
-    string += "Pairs List Length: " + str(len(noteutil.pairs_list)) + "\n"
+
+    string += "Pairs List Terms" + "\n"
+    string += "----------------" + "\n"
+    for pair in noteutil.pairs:
+        string += "\t" + pair.term + "\n"
+    string += "----------------" + "\n"
+
+    string += "Heading Character: " + noteutil.heading_char + "\n"
+    string += "Levels: " + str(noteutil.levels) + "\n"
+    string += "Headings" + "\n"
+    string += "--------" + "\n"
+    for gname, anames in noteutil.headings.items():     # General name, Actual names
+        string += "\t" + gname + ": " + str(list(map(lambda x: x.heading_name, anames))) + "\n"
+    string += "--------" + "\n"
+    string += "Heading Order" + "\n"
+    string += "-------------" + "\n"
+    for note in noteutil.heading_order:
+        string += "\tLevel " + str(note.level) + ": \t" + note.heading_name + "\n"
+    string += "-------------" + "\n"
     return string
 
 
@@ -24,7 +42,17 @@ def test_note(note):
     string += "Note Index: {!s:<5}\t\t".format(note.nindex)
     string += "Term: {!s:<10}\t\t".format(note.term[:10] if note.term else None)
     string += "Definition: {!s:<10}\t\t".format(note.definition[:10] if note.definition else None)
-    string += "Separator: {0}\t\t".format(note.separator)
+    string += "Separator: {!s:<5}\t\t".format(note.separator)
+    string += "Heading Character: {!s:<5}\t\t".format(note.heading_char)
+    string += "Level: {!s:<3}\t\t".format(note.level)
+    string += "Heading: {!s:<10}\t\t".format(note.heading)
+    string += "Heading Name: {!s:<10}\t\t".format(note.heading_name[:10] if note.heading_name else None)
+    string += "Beginning Note Index: {!s:<5}\t\t".format(note.begin_nindex)
+    string += "Ending Note Index: {!s:<5}\t\t".format(note.end_nindex)
+    if note.notes:
+        string += "Notes List: " + str(list(map(lambda x: x.nindex, note.notes)))
+    else:
+        string += "Notes List: None"
     return string
 
 
