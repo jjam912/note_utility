7dc9d08e223d39544b671c3c4db70b8a462ff3c2
jjam912 <new.jjam912@gmail.com>
2019-03-31 09:30:50 -0400

    Added HeadingJump

--- noteutil2/errors.py
+++ noteutil2/errors.py
@@ -18,3 +18,12 @@ class ExtraSeparator(NoteError):
 class NoDefinition(NoteError):
     def __init__(self, content):
         super().__init__("There was no text after the separator in the line content: {0}".format(content))
+
+
+class HeadingJump(NoteError):
+    def __init__(self, previous_level, current_level):
+        super().__init__("The heading level jumped down 2 or more levels: From level {0} to {1}".
+                         format(previous_level, current_level))
+
+
+
