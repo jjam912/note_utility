6978677f9f9b1e02bd140b5e10340a53f8d6e99a
jjam912 <new.jjam912@gmail.com>
2019-04-05 17:44:24 -0400

    Updated edit test to reflect new edit method

--- noteutil2/testing/edit_test.py
+++ noteutil2/testing/edit_test.py
@@ -1,14 +1,3 @@
-"""The goal here should be that we can edit our notes through NoteUtil in the most convenient way possible.
-We should be able to:
-    1. Change content, terms, and definitions.
-        One should not be able to change terms or definitions, because changing content affects those two.
-        Therefore, NoteUtil should only allow changes to content and not term nor definition.
-    2. Add and remove notes
-    3. Append notes by parsing new content.
-
-"""
-
-
 from noteutil2.noteutil import NoteUtil
 from noteutil2.comparisons import CompareOptions
 import os
@@ -50,11 +39,11 @@ def test_note_list(note_list):
     return "\t\t".join((list(map(test_note, note_list))))
 
 
-def before_after_edit(note, **kwargs):
+def before_after_edit(note, content):
     print("Before:\n"
           "-------\n" + test_note(note) + "\n" +
           "-------")
-    noteutil.edit(note, **kwargs)
+    noteutil.edit(note, content)
     print("After:\n"
           "------\n" + test_note(note) + "\n" +
           "------")
@@ -66,7 +55,7 @@ noteutil = NoteUtil("edit_config.txt")
 print(test_noteutil(noteutil))
 
 n1 = noteutil.get(nindex=0)
-before_after_edit(n1, content="Wumbo", term="Hi", definition="Bye")
+before_after_edit(n1, "Wumbo")
 
 print(test_noteutil(noteutil))
 
