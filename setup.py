999c7febbcacafed33935fdb708c85f0c7d7a7b3
jjam912 <new.jjam912@gmail.com>
2019-07-23 08:10:09 -0400

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="noteutil",
-    version="0.0.3",
+    version="0.0.4",
     author="James S. Wang",
     author_email="jjameswwang@gmail.com",
     description="A package for handling specially written notes",
