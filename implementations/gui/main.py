c4a3d805b0e002766e6dc2f219a6310072d683ab
jjam912 <new.jjam912@gmail.com>
2019-08-01 07:51:12 -0400

    Separated popup menus

--- implementations/gui/main.py
+++ implementations/gui/main.py
@@ -13,24 +13,9 @@ class Main:
         self.noteutil_label = None
         self.continue_label = None
         self.init_labels()
-        self.popup_menu = None
+        self.text_popup = None
+        self.entry_popup = None
         self.init_binds()
-
-    def init_popup_menu(self, event):
-        self.popup_menu = tk.Menu(event.widget, tearoff=False)
-        self.popup_menu.add_command(label="Undo", accelerator="Ctrl+Z",
-                                    command=lambda: event.widget.event_generate("<<Undo>>"))
-        self.popup_menu.add_command(label="Redo", accelerator="Ctrl+Y",
-                                    command=lambda: event.widget.event_generate("<<Redo>>"))
-        self.popup_menu.add_separator()
-        self.popup_menu.add_command(label="Cut", accelerator="Ctrl+X",
-                                    command=lambda: event.widget.event_generate("<<Cut>>"))
-        self.popup_menu.add_command(label="Copy", accelerator="Ctrl+C",
-                                    command=lambda: event.widget.event_generate("<<Copy>>"))
-        self.popup_menu.add_command(label="Paste", accelerator="Ctrl+V",
-                                    command=lambda: event.widget.event_generate("<<Paste>>"))
-        self.popup_menu.add_command(label="Select All", accelerator="Ctrl+A",
-                                    command=lambda: event.widget.event_generate("<<Select All>>"))
 
     def init_labels(self):
         self.noteutil_label = tk.Label(self.root, text="NoteUtil", pady=100, fg="blue",
@@ -41,6 +26,33 @@ class Main:
         self.noteutil_label.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
         self.continue_label.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
 
+    def init_text_popup(self, event):
+        self.text_popup = tk.Menu(event.widget, tearoff=False)
+        self.text_popup.add_command(label="Undo", accelerator="Ctrl+Z",
+                                    command=lambda: event.widget.event_generate("<<Undo>>"))
+        self.text_popup.add_command(label="Redo", accelerator="Ctrl+Y",
+                                    command=lambda: event.widget.event_generate("<<Redo>>"))
+        self.text_popup.add_separator()
+        self.text_popup.add_command(label="Cut", accelerator="Ctrl+X",
+                                    command=lambda: event.widget.event_generate("<<Cut>>"))
+        self.text_popup.add_command(label="Copy", accelerator="Ctrl+C",
+                                    command=lambda: event.widget.event_generate("<<Copy>>"))
+        self.text_popup.add_command(label="Paste", accelerator="Ctrl+V",
+                                    command=lambda: event.widget.event_generate("<<Paste>>"))
+        self.text_popup.add_command(label="Select All", accelerator="Ctrl+A",
+                                    command=lambda: event.widget.event_generate("<<Select All>>"))
+
+    def init_entry_popup(self, event):
+        self.entry_popup = tk.Menu(event.widget, tearoff=False)
+        self.entry_popup.add_command(label="Cut", accelerator="Ctrl+X",
+                                     command=lambda: event.widget.event_generate("<<Cut>>"))
+        self.entry_popup.add_command(label="Copy", accelerator="Ctrl+C",
+                                     command=lambda: event.widget.event_generate("<<Copy>>"))
+        self.entry_popup.add_command(label="Paste", accelerator="Ctrl+V",
+                                     command=lambda: event.widget.event_generate("<<Paste>>"))
+        self.entry_popup.add_command(label="Select All", accelerator="Ctrl+A",
+                                     command=lambda: event.widget.event_generate("<<Select All>>"))
+
     def init_binds(self):
         self.root.bind_class(tk.Text.__name__, "<<Select All>>", lambda e: e.widget.tag_add(tk.SEL, 1.0, tk.END))
         self.root.bind_class(tk.Text.__name__, "<<Select All>>", lambda e: e.widget.tag_add(tk.SEL, 1.0, tk.END))
@@ -50,17 +62,22 @@ class Main:
         self.root.bind_class(tk.Text.__name__, "<Control-y>", lambda e: e.widget.event_generate("<<Redo>>"))
         self.root.bind_class(tk.Text.__name__, "<Control-A>", lambda e: e.widget.event_generate("<<Select All>>"))
         self.root.bind_class(tk.Text.__name__, "<Control-a>", lambda e: e.widget.event_generate("<<Select All>>"))
-        self.root.bind_class(tk.Text.__name__, "<Button-3>", self.popup)
+        self.root.bind_class(tk.Text.__name__, "<Button-3>", self.open_text_popup)
 
         self.root.bind_class(tk.Entry.__name__, "<<Select All>>", lambda e: e.widget.select_range(0, tk.END))
         self.root.bind_class(tk.Entry.__name__, "<<Select All>>", lambda e: e.widget.select_range(0, tk.END))
         self.root.bind_class(tk.Entry.__name__, "<Control-A>", lambda e: e.widget.event_generate("<<Select All>>"))
         self.root.bind_class(tk.Entry.__name__, "<Control-a>", lambda e: e.widget.event_generate("<<Select All>>"))
-        self.root.bind_class(tk.Entry.__name__, "<Button-3>", self.popup)
+        self.root.bind_class(tk.Entry.__name__, "<Button-3>", self.open_entry_popup)
 
-    def popup(self, event):
-        self.init_popup_menu(event)
-        self.popup_menu.tk_popup(event.x_root, event.y_root)
+    def open_text_popup(self, event):
+        self.init_text_popup(event)
+        self.text_popup.tk_popup(event.x_root, event.y_root)
+        return "break"
+
+    def open_entry_popup(self, event):
+        self.init_entry_popup(event)
+        self.entry_popup.tk_popup(event.x_root, event.y_root)
         return "break"
 
     def clear(self):
