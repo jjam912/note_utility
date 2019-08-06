edb653e19294840a9b6bb0907f80898f1a31514a
JJamesWWang <JJamesWWang@gmail.com>
2019-08-06 07:55:06 -0400

    Turned main into the tk.Tk

--- implementations/gui/main.py
+++ implementations/gui/main.py
@@ -1,30 +1,13 @@
 """The starting program that greets the user and then continues to the configurator."""
 import tkinter as tk
 import tkinter.font as tkfont
-from configurator import ConfiguratorView
+from welcomer import Welcomer
 
 
 class Main(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title("NoteUtil")
-        self.bind("<Button-1>", self.to_configurator)
-
-        self.noteutil_label = None
-        self.continue_label = None
-        self.init_labels()
-        self.text_popup = None
-        self.entry_popup = None
         self.init_binds()
-
-    def init_labels(self):
-        self.noteutil_label = tk.Label(self, text="NoteUtil", pady=100, fg="blue",
-                                       font=tkfont.Font(family="Ubuntu", size=192, weight="bold", underline=True))
-        self.continue_label = tk.Label(self, text="Click anywhere to\ncontinue", pady=50,
-                                       font=tkfont.Font(family="Ubuntu", size=96))
-
-        self.noteutil_label.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
-        self.continue_label.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
 
     def init_text_popup(self, event):
         self.text_popup = tk.Menu(event.widget, tearoff=False)
@@ -80,19 +63,10 @@ class Main(tk.Tk):
         self.entry_popup.tk_popup(event.x_root, event.y_root)
         return "break"
 
-    def clear(self):
-        """Removes all of the root's children"""
-        self.unbind("<Button-1>")
-        for widget in self.winfo_children():
-            widget.destroy()
-
-    def to_configurator(self, event=None):
-        """Clears the root and then instantiates the ConfiguratorView."""
-        self.clear()
-        ConfiguratorView(self)
-
 
 if __name__ == "__main__":
-    app = Main()
-    app.geometry("1600x900+160+90")
-    app.mainloop()
+    gui = Main()
+    Welcomer(gui)
+
+    gui.geometry("1600x900+160+90")
+    gui.mainloop()
