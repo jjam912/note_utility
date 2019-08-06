23ed34b473963e1a35b3a7682699324d424e26dc
JJamesWWang <JJamesWWang@gmail.com>
2019-08-06 07:45:37 -0400

    Changed main to inherit tk.Tk

--- implementations/gui/main.py
+++ implementations/gui/main.py
@@ -4,11 +4,11 @@ import tkinter.font as tkfont
 from configurator import ConfiguratorView
 
 
-class Main:
-    def __init__(self, root: tk.Tk):
-        self.root = root
-        self.root.title("NoteUtil")
-        self.root.bind("<Button-1>", self.to_configurator)
+class Main(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("NoteUtil")
+        self.bind("<Button-1>", self.to_configurator)
 
         self.noteutil_label = None
         self.continue_label = None
@@ -18,9 +18,9 @@ class Main:
         self.init_binds()
 
     def init_labels(self):
-        self.noteutil_label = tk.Label(self.root, text="NoteUtil", pady=100, fg="blue",
+        self.noteutil_label = tk.Label(self, text="NoteUtil", pady=100, fg="blue",
                                        font=tkfont.Font(family="Ubuntu", size=192, weight="bold", underline=True))
-        self.continue_label = tk.Label(self.root, text="Click anywhere to\ncontinue", pady=50,
+        self.continue_label = tk.Label(self, text="Click anywhere to\ncontinue", pady=50,
                                        font=tkfont.Font(family="Ubuntu", size=96))
 
         self.noteutil_label.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
@@ -54,21 +54,21 @@ class Main:
                                      command=lambda: event.widget.event_generate("<<Select All>>"))
 
     def init_binds(self):
-        self.root.bind_class(tk.Text.__name__, "<<Select All>>", lambda e: e.widget.tag_add(tk.SEL, 1.0, tk.END))
-        self.root.bind_class(tk.Text.__name__, "<<Select All>>", lambda e: e.widget.tag_add(tk.SEL, 1.0, tk.END))
-        self.root.bind_class(tk.Text.__name__, "<Control-Z>", lambda e: e.widget.event_generate("<<Undo>>"))
-        self.root.bind_class(tk.Text.__name__, "<Control-z>", lambda e: e.widget.event_generate("<<Undo>>"))
-        self.root.bind_class(tk.Text.__name__, "<Control-Y>", lambda e: e.widget.event_generate("<<Redo>>"))
-        self.root.bind_class(tk.Text.__name__, "<Control-y>", lambda e: e.widget.event_generate("<<Redo>>"))
-        self.root.bind_class(tk.Text.__name__, "<Control-A>", lambda e: e.widget.event_generate("<<Select All>>"))
-        self.root.bind_class(tk.Text.__name__, "<Control-a>", lambda e: e.widget.event_generate("<<Select All>>"))
-        self.root.bind_class(tk.Text.__name__, "<Button-3>", self.open_text_popup)
+        self.bind_class(tk.Text.__name__, "<<Select All>>", lambda e: e.widget.tag_add(tk.SEL, 1.0, tk.END))
+        self.bind_class(tk.Text.__name__, "<<Select All>>", lambda e: e.widget.tag_add(tk.SEL, 1.0, tk.END))
+        self.bind_class(tk.Text.__name__, "<Control-Z>", lambda e: e.widget.event_generate("<<Undo>>"))
+        self.bind_class(tk.Text.__name__, "<Control-z>", lambda e: e.widget.event_generate("<<Undo>>"))
+        self.bind_class(tk.Text.__name__, "<Control-Y>", lambda e: e.widget.event_generate("<<Redo>>"))
+        self.bind_class(tk.Text.__name__, "<Control-y>", lambda e: e.widget.event_generate("<<Redo>>"))
+        self.bind_class(tk.Text.__name__, "<Control-A>", lambda e: e.widget.event_generate("<<Select All>>"))
+        self.bind_class(tk.Text.__name__, "<Control-a>", lambda e: e.widget.event_generate("<<Select All>>"))
+        self.bind_class(tk.Text.__name__, "<Button-3>", self.open_text_popup)
 
-        self.root.bind_class(tk.Entry.__name__, "<<Select All>>", lambda e: e.widget.select_range(0, tk.END))
-        self.root.bind_class(tk.Entry.__name__, "<<Select All>>", lambda e: e.widget.select_range(0, tk.END))
-        self.root.bind_class(tk.Entry.__name__, "<Control-A>", lambda e: e.widget.event_generate("<<Select All>>"))
-        self.root.bind_class(tk.Entry.__name__, "<Control-a>", lambda e: e.widget.event_generate("<<Select All>>"))
-        self.root.bind_class(tk.Entry.__name__, "<Button-3>", self.open_entry_popup)
+        self.bind_class(tk.Entry.__name__, "<<Select All>>", lambda e: e.widget.select_range(0, tk.END))
+        self.bind_class(tk.Entry.__name__, "<<Select All>>", lambda e: e.widget.select_range(0, tk.END))
+        self.bind_class(tk.Entry.__name__, "<Control-A>", lambda e: e.widget.event_generate("<<Select All>>"))
+        self.bind_class(tk.Entry.__name__, "<Control-a>", lambda e: e.widget.event_generate("<<Select All>>"))
+        self.bind_class(tk.Entry.__name__, "<Button-3>", self.open_entry_popup)
 
     def open_text_popup(self, event):
         self.init_text_popup(event)
@@ -82,18 +82,17 @@ class Main:
 
     def clear(self):
         """Removes all of the root's children"""
-        for widget in self.root.winfo_children():
+        self.unbind("<Button-1>")
+        for widget in self.winfo_children():
             widget.destroy()
 
     def to_configurator(self, event=None):
         """Clears the root and then instantiates the ConfiguratorView."""
-        self.root.unbind("<Button-1>")
         self.clear()
-        ConfiguratorView(self.root)
+        ConfiguratorView(self)
 
 
 if __name__ == "__main__":
-    gui = tk.Tk()
-    app = Main(gui)
-    gui.geometry("1600x900+160+90")
-    gui.mainloop()
+    app = Main()
+    app.geometry("1600x900+160+90")
+    app.mainloop()
