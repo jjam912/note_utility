1aec980a66bc923c73413b17b59503294cd0925e
JJamesWWang <JJamesWWang@gmail.com>
2019-08-06 20:52:28 -0400

    Remove redundant call to open_config

--- implementations/gui/configurator.py
+++ implementations/gui/configurator.py
@@ -38,10 +38,6 @@ class ConfiguratorView:
         self.init_actions_frame()
 
         self.controller.read_settings()
-        if noteutil is not None and quiz is not None and leitner is not None:
-            with open(noteutil.config_file, mode="r") as f:
-                self.controller.on_open_config(f)
-
         self.root.protocol("WM_DELETE_WINDOW", self.controller.on_close)
 
     def init_menu_bar(self):
