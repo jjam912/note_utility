b6e80bcba005097691f48e45e5c6320db6900808
James S. Wang <JJamesWWang@gmail.com>
2019-08-09 10:08:40 -0400

    Create templates for tests

---
+++ tests/test_leitner.py
@@ -0,0 +1,11 @@
+import noteutil as nu
+
+
+basic_noteutil = nu.NoteUtil("test_data/basic_config.txt", refresh=True)
+pair_noteutil = nu.NoteUtil("test_data/pair_config.txt", refresh=True)
+heading_noteutil = nu.NoteUtil("test_data/heading_config.txt", refresh=True)
+extension_noteutil = nu.NoteUtil("test_data/extension_config.txt", refresh=True)
+category_noteutil = nu.NoteUtil("test_data/category_config.txt", refresh=True)
+all1_noteutil = nu.NoteUtil("test_data/all1_config.txt", refresh=True)
+all2_noteutil = nu.NoteUtil("test_data/all2_config.txt", refresh=True)
+all3_noteutil = nu.NoteUtil("test_data/all3_config.txt", refresh=True)
